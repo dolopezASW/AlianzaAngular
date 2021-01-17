2eda285e6ba52c97c0500b4e91c54145f5ebf2b2
dolopez <dolopez@asesoftware.com>
2021-01-17 14:26:24 -0500

    Ajustes a pantalla de clientes.

--- src/app/admin-clientes/admin-clientes.component.ts
+++ src/app/admin-clientes/admin-clientes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ClientService } from '../services/client.service';
 import { Client } from 'src/app/models/client';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import {NgbModal, ModalDismissReasons, NgbModalOptions} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-admin-clientes',
@@ -12,8 +13,16 @@ export class AdminClientesComponent implements OnInit {
 
   
   clientList:Array<Client>=[];
+
+  closeResult: string='';
+  modalOptions:NgbModalOptions;
   
-  constructor(private clientService: ClientService) { }
+  constructor(private clientService: ClientService, private modalService: NgbModal) {
+    this.modalOptions = {
+      backdrop:'static',
+      backdropClass:'customBackdrop'
+    }
+   }
 
   ngOnInit(): void {
     this.getClients();
@@ -26,8 +35,21 @@ export class AdminClientesComponent implements OnInit {
     });
   }
 
-  open() {
-    
+  open(content:any) {
+    this.modalService.open(content, this.modalOptions).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
